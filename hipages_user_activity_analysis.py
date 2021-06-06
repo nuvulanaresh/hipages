52328043914a4da9c324a320cbb8c1db2584060f
nuvulanaresh <31900328+nuvulanaresh@users.noreply.github.com>
2021-06-07 00:21:04 +1000

    updates

--- hipages_user_activity_analysis.py
+++ hipages_user_activity_analysis.py
@@ -4,14 +4,28 @@ from pyspark.sql import SparkSession, SQLContext
 from pyspark.sql.types import *
 from pyspark.sql.functions import *
 
-context = SparkContext('local[*]')
-spark = SparkSession(context)
-#context.setLogLevel('WARN')
-#scope for parameterization
+sc = SparkContext(master="local[*]",appName="hipages_process_json")
+spark=SparkSession\
+    .builder\
+    .appName("hipages_process_json")\
+    .getOrCreate()
+
 jsonpath="/home/jovyan/work/data/source_event_data.json"
+
+jsonschema = StructType([
+  StructField("action", StringType(), True),
+  StructField("event_id", StringType(), True),
+  StructField("timestamp", StringType(), True),
+  StructField("url", StringType(), True),
+  StructField("user", StructType([
+    StructField("id", LongType(), True),
+    StructField("ip", StringType(), True),
+    StructField("session_id", StringType(), True)]))
+])
+
 user_activity_df = (spark.read\
     .option("header","false")\
-    .option("inferSchema","true")\
+    .schema(jsonschema)\
     .json(jsonpath)
     .select(col("event_id"),col("user.id").alias("user_id"),col("action").alias("activity"),"url",col("timestamp").alias("time_stamp"))\
     .withColumn("url_level1",concat(split(("url"),'\\.').getItem(1),lit('.'),split(("url"),'\\.').getItem(2)))\
@@ -20,18 +34,13 @@ user_activity_df = (spark.read\
     .withColumn("url_level3",split(("url_sub_level"),"/").getItem(2))\
     .select("user_id","time_stamp","url_level1","url_level2","url_level3","activity")
     )
-#print(user_activity_df.show(5))
+
 #Writing the User Activity DataFrame to csv file
 user_activity_df.write\
     .option("header","true")\
     .option("sep",",")\
     .mode("overwrite")\
     .csv("/home/jovyan/work/data/user_activity")
-
-# user_activity_df.write\
-#     .format("csv")\
-#     .mode("overwrite")\
-#     .saveAsTable("hipages_user_activity")
 
 hrly_granular_activtiy_df = (user_activity_df\
     .withColumn("time_bucket", from_unixtime(unix_timestamp(col("time_stamp"), "MM/dd/yyyy HH:mm:ss"), "yyyyMMddHH"))\
@@ -44,8 +53,3 @@ hrly_granular_activtiy_df.write\
     .option("sep",",")\
     .mode("overwrite")\
     .csv("/home/jovyan/work/data/hrly_granular_activtiy")
-
-# hrly_granular_activtiy_df.write\
-#     .format("csv")\
-#     .mode("overwrite")\
-#     .saveAsTable("hipages_hrly_granular_activity")
