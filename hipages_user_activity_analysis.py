3acc1fc121e7e4101e50faf2f22c747699de4acc
nuvulanaresh <31900328+nuvulanaresh@users.noreply.github.com>
2021-06-07 09:57:49 +1000

    Update hipages_user_activity

--- hipages_user_activity_analysis.py
+++ hipages_user_activity_analysis.py
@@ -1,17 +1,21 @@
+#Import required packages
 import pyspark
 from pyspark import SparkContext, SparkConf
 from pyspark.sql import SparkSession, SQLContext
 from pyspark.sql.types import *
 from pyspark.sql.functions import *
 
+#Set SparkContext
 sc = SparkContext(master="local[*]",appName="hipages_process_json")
 spark=SparkSession\
     .builder\
     .appName("hipages_process_json")\
     .getOrCreate()
 
-jsonpath="/home/jovyan/work/data/source_event_data.json"
+# Input path for the data source
+jsonpath="/home/jovyan/source_event_data.json"
 
+# Define Schema for the json data source
 jsonschema = StructType([
   StructField("action", StringType(), True),
   StructField("event_id", StringType(), True),
@@ -23,11 +27,15 @@ jsonschema = StructType([
     StructField("session_id", StringType(), True)]))
 ])
 
+#Create DataFrame and performing the data transformations
 user_activity_df = (spark.read\
+    #Header set to false as there is no header for the data source
     .option("header","false")\
+    #Inferring Schema from the schema created above
     .schema(jsonschema)\
     .json(jsonpath)
     .select(col("event_id"),col("user.id").alias("user_id"),col("action").alias("activity"),"url",col("timestamp").alias("time_stamp"))\
+    #Below transformations extract the desired data from the URL column
     .withColumn("url_level1",concat(split(("url"),'\\.').getItem(1),lit('.'),split(("url"),'\\.').getItem(2)))\
     .withColumn("url_sub_level",split(("url"),'\\.').getItem(3))\
     .withColumn("url_level2",split(("url_sub_level"),"/").getItem(1))\
@@ -35,21 +43,22 @@ user_activity_df = (spark.read\
     .select("user_id","time_stamp","url_level1","url_level2","url_level3","activity")
     )
 
-#Writing the User Activity DataFrame to csv file
+#Write the transformed User Activity DataFrame to csv file
 user_activity_df.write\
     .option("header","true")\
     .option("sep",",")\
     .mode("overwrite")\
-    .csv("/home/jovyan/work/data/user_activity")
+    .csv("/home/jovyan/user_activity")
 
-hrly_granular_activtiy_df = (user_activity_df\
+# Create DataFrame and calculate the hourly user activity
+hrly_granular_activity_df = (user_activity_df\
     .withColumn("time_bucket", from_unixtime(unix_timestamp(col("time_stamp"), "MM/dd/yyyy HH:mm:ss"), "yyyyMMddHH"))\
     .groupBy("time_bucket","url_level1","url_level2","activity")\
     .agg(count("activity").alias("activity_count"),countDistinct("user_id").alias("user_count"))
     )
-
-hrly_granular_activtiy_df.write\
+#Write the hourly user activity to the csv
+hrly_granular_activity_df.write\
     .option("header","true")\
     .option("sep",",")\
     .mode("overwrite")\
-    .csv("/home/jovyan/work/data/hrly_granular_activtiy")
+    .csv("/home/jovyan/hrly_granular_activity")
